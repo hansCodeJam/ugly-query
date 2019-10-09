d1fc072b127b49505cb83a48a7cae6f516a3d815
Hans Vos <hans.vos@codeimmersives.com>
2019-10-08 22:03:25 -0400

    commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const uglyBackground = document.querySelector('body');
+uglyBackground.style.backgroundImage = "url(https://cache.desktopnexus.com/cropped-wallpapers/866/866750-1440x900-[DesktopNexus.com].jpg?st=ITGugRMGFszLUGpbAnhAEA&e=1570565952)";
+
+const uglyH1 = document.querySelector('h1');
+uglyH1.style.color = 'pink';
+
+const colorP = document.querySelector('#green');
+colorP.style.color = 'green';
+colorP.style.border = 'thick solid #0000FF';
+
+const opacityList = document.querySelector('#opacity')
+opacityList.style.opacity = '0.5';
+opacityList.style.color = 'white';
+
+const backgroundTextArea = document.querySelector('textarea');
+backgroundTextArea.style.backgroundColor = 'red';
+
+const uglyFont = document.querySelector('#uglyFont');
+uglyFont.style.fontFamily = "Impact,Charcoal,sans-serif";
+uglyFont.style.border = "thick solid white";
+uglyFont.style.backgroundColor = 'blue';  
+
+
+const uglyYellowInput = document.querySelector(".yellow");
+uglyYellowInput.style.backgroundColor = 'yellow';
+
+const uglyBlueInput = document.querySelector(".blue");
+uglyBlueInput.style.backgroundColor = 'blue';
+
+
+
+
+// document.getElementById("myDiv").style.border = "thick solid #0000FF";
+
+
+
