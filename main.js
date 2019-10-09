de6ebb8428becd27a8f7eca0d6c6a404d7d1065f
Hans Vos <hans.vos@codeimmersives.com>
2019-10-09 09:17:32 -0400

    commit

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ uglyBackground.style.backgroundImage = "url(https://cache.desktopnexus.com/cropp
 
 const uglyH1 = document.querySelector('h1');
 uglyH1.style.color = 'pink';
+uglyH1.style.border = 'thick solid white'
+uglyH1.style.borderRadius = '50%';
+uglyH1.style.backgroundImage = "url(http://www.tioxic.com/wp-content/uploads/trex_4.jpg)";
+uglyH1.style.opacity = '0.6';
 
 const colorP = document.querySelector('#green');
 colorP.style.color = 'green';
 colorP.style.border = 'thick solid #0000FF';
+colorP.style.borderRadius = '50px 20px';
 
 const opacityList = document.querySelector('#opacity')
 opacityList.style.opacity = '0.5';
@@ -19,6 +24,7 @@ const uglyFont = document.querySelector('#uglyFont');
 uglyFont.style.fontFamily = "Impact,Charcoal,sans-serif";
 uglyFont.style.border = "thick solid white";
 uglyFont.style.backgroundColor = 'blue';  
+uglyFont.style.borderRadius = '25px';
 
 
 const uglyYellowInput = document.querySelector(".yellow");
@@ -27,6 +33,8 @@ uglyYellowInput.style.backgroundColor = 'yellow';
 const uglyBlueInput = document.querySelector(".blue");
 uglyBlueInput.style.backgroundColor = 'blue';
 
+const font = document.querySelector('#font');
+font.style.fontStretch = 'expanded';
 
 
 
