f1c4102d9c99eb446b0b979c9da47c35965c4c5e
Hans Vos <hans.vos@codeimmersives.com>
2019-10-09 10:53:40 -0400

    commit

--- main.js
+++ main.js
@@ -5,26 +5,28 @@ const uglyH1 = document.querySelector('h1');
 uglyH1.style.color = 'pink';
 uglyH1.style.border = 'thick solid white'
 uglyH1.style.borderRadius = '50%';
-uglyH1.style.backgroundImage = "url(http://www.tioxic.com/wp-content/uploads/trex_4.jpg)";
+uglyH1.style.backgroundImage = "url(https://www.animatedimages.org/data/media/187/animated-dinosaur-image-0001.gif)";
 uglyH1.style.opacity = '0.6';
 
 const colorP = document.querySelector('#green');
 colorP.style.color = 'green';
 colorP.style.border = 'thick solid #0000FF';
 colorP.style.borderRadius = '50px 20px';
+colorP.style.backgroundImage = "url(https://www.animatedimages.org/data/media/710/animated-dice-image-0015.gif)"
 
 const opacityList = document.querySelector('#opacity')
 opacityList.style.opacity = '0.5';
 opacityList.style.color = 'white';
 
 const backgroundTextArea = document.querySelector('textarea');
-backgroundTextArea.style.backgroundColor = 'red';
+backgroundTextArea.style.backgroundImage = "url(https://www.animatedimages.org/data/media/118/animated-robot-image-0051.gif)";
 
 const uglyFont = document.querySelector('#uglyFont');
 uglyFont.style.fontFamily = "Impact,Charcoal,sans-serif";
 uglyFont.style.border = "thick solid white";
 uglyFont.style.backgroundColor = 'blue';  
 uglyFont.style.borderRadius = '25px';
+uglyFont.style.backgroundImage = "url(https://www.animatedimages.org/data/media/209/animated-cat-image-0394.gif)"
 
 
 const uglyYellowInput = document.querySelector(".yellow");
